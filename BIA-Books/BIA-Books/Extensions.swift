692ae175e4e9f7fe7f591da1f4dc4fd4516c7166
aLikan-jpg <alikinnikita2002@gmail.com>
2023-03-17 18:18:31 +0300

    first version of detail screen UI

--- BIA-Books/BIA-Books/Extensions.swift
+++ BIA-Books/BIA-Books/Extensions.swift
@@ -29,3 +29,16 @@ extension UITableViewCell {
         self.selectionStyle = .none
     }
 }
+extension UIStackView {
+    func customize(backgroundColor: UIColor = .clear, radiusSize: CGFloat = 0) {
+        let subView = UIView(frame: bounds)
+        subView.backgroundColor = backgroundColor
+        subView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        insertSubview(subView, at: 0)
+
+        subView.layer.cornerRadius = radiusSize
+        subView.layer.masksToBounds = true
+        subView.clipsToBounds = true
+    }
+}
+
--- BIA-Books/BIA-Books/Modules/Books List/View/Cell/BooksTableViewCell.swift
+++ BIA-Books/BIA-Books/Modules/Books List/View/Cell/BooksTableViewCell.swift
@@ -12,36 +12,27 @@ class BooksTableViewCell: UITableViewCell {
     @IBOutlet weak var bookName: UILabel!
     @IBOutlet weak var cardView: UIView!
     
-    let selectedColorView = UIView()
-    
     @IBOutlet weak var language: UILabel!
     @IBOutlet weak var bookStatus: UILabel!
+    
+    @IBOutlet weak var dateStack: UIStackView!
+    @IBOutlet weak var languageStack: UIStackView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        cardView.aplyShadow(cornerRadius: 12)
         setUp()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
     
-    private func setShadow(stack : UIStackView) {
-        stack.layer.cornerRadius = 12
-        stack.layer.masksToBounds = false
-        stack.layer.shadowRadius = 4.0
-        stack.layer.shadowOpacity = 0.30
-        stack.layer.shadowColor = UIColor.black.cgColor
-        stack.layer.shadowOffset = CGSize(width: 0, height: 5)
-    }
     private func setUp() {
-        roundCornersForUILabels(label: language)
-        roundCornersForUILabels(label: bookStatus)
-        roundCornersForUILabels(label: bookName)
+        cardView.aplyShadow(cornerRadius: 12)
+        roundCornersForUILabels(stack: dateStack)
+        roundCornersForUILabels(stack: languageStack)
         
     }
-    private func roundCornersForUILabels(label: UILabel) {
-        label.layer.cornerRadius = 6
-        label.layer.cornerCurve = .continuous
+    
+    private func roundCornersForUILabels(stack: UIStackView) {
+        stack.layer.cornerRadius = 6
+        stack.layer.cornerCurve = .continuous
     }
 }
