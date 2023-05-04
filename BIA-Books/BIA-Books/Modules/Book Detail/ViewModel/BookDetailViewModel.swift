f74c0750cae561ac05acd3e4e3d2565923d8f758
aLikan-jpg <alikinnikita2002@gmail.com>
2023-05-04 13:36:51 +0300

    request for bookInfo added

--- BIA-Books/BIA-Books/Modules/Book Detail/ViewModel/BookDetailViewModel.swift
+++ BIA-Books/BIA-Books/Modules/Book Detail/ViewModel/BookDetailViewModel.swift
@@ -9,12 +9,20 @@ import Foundation
 
 
 class BookDetailViewModel  :ViewModelDetailType  {
+    private var fetcher = NetworkDataFetcher(networking: NetworkService())
+
     func numberOfRows() -> Int {
         return labels.count
     }
     
     
     var labels = ["Дизайн",  "Разработка", "1С"]
+    
+    func loadBookInfo() {
+        fetcher.getBookInfo(params: ["bookId" : "6450fdf9f2393509a5d29c1b"]) { response in
+            
+        }
+    }
     
 
     
