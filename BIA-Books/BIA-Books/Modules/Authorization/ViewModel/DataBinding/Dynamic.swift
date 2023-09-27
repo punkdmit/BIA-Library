819ced9418217b93fef41d29bfb0cb5be72b0f14
punkdmit <71826735+punkdmit@users.noreply.github.com>
2023-09-27 21:51:35 +0300

    no message

--- BIA-Books/BIA-Books/Modules/Authorization/ViewModel/DataBinding/Dynamic.swift
+++ BIA-Books/BIA-Books/Modules/Authorization/ViewModel/DataBinding/Dynamic.swift
@@ -9,15 +9,22 @@ import Foundation
 
 class Dynamic <T> {
     typealias Listener = (T) -> Void
+    
+    // Хранит замыкание слушателя, которое будет вызываться при изменении значения
     private var listener : Listener?
+    
+    // Позволяет привязать слушателя к объекту Dynamic
     func bind(_ listener : Listener?) {
         self.listener = listener
     }
+    
+    // Когда значение устанавливается, код вызывает замыкание слушателя и передает новое значение
     var value : T {
         didSet {
             listener?(value)
         }
     }
+    
     init(_ v : T) {
         value = v
     }
